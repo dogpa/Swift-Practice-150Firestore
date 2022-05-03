5e2f652dd0b3719ad65d5bae5f814fe2ab63e75d
Dogpa <dogpa0130@gmail.com>
2022-05-03 14:11:57 +0800

    Initial Commit

---
+++ SwiftPractice150Firestore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPractice150Firestore
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPractice150Firestore/SwiftPractice150FirestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPractice150FirestoreApp.swift
+//  SwiftPractice150Firestore
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/3.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPractice150FirestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
